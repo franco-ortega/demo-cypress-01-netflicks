374ffdd7c698556a5d15b8df216aaa047937e0d2
Franco Ortega <francopdx@gmail.com>
2023-08-08 14:47:26 -0700

    create Fun component and corresponding component test

---
+++ src/Fun.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Fun = ({ propsValue }) => {
+  return (
+    <div className='funClass'>
+      <h1>Fun</h1>
+      <p>{propsValue}</p>
+    </div>
+  );
+};
+
+export default Fun;
